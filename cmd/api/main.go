53005fed14c92b2a9bfacb3851fd73d46d5a4cc5
StupidAfCoder <malikyash2007@gmail.com>
2026-04-29 19:00:56 +0530

    Added the main function

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"trinethra/internal/handler"
+)
 
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/analyze", handler.Analyze)
+
+	http.ListenAndServe(":8080", nil)
 }
